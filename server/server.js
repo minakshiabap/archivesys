68ddafc086791e07ee60028ec1b61c90ddc905ab
Bajaj <a.bajaj@sap.com>
2019-10-08 22:56:28 +0530

    base functions

---
+++ server/server.js
@@ -0,0 +1,21 @@
+var loopback = require('loopback');
+var boot = require('loopback-boot');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var fileUpload = require('express-fileupload');
+var fs = require('fs');
+var xlstojson = require("xls-to-json-lc");
+var xlsxtojson = require("xlsx-to-json-lc");
+var express = require('express');
+var fs = require('fs');
+var app = express();
+app = module.exports = loopback();
+// parse application/json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(session({
+	secret: 'anubhavApp'
+}));
+app.use(fileUpload());
