c7fb8701007726a0aa023f59b52c56e45d81a6a5
Bajaj <a.bajaj@sap.com>
2019-10-13 16:45:56 +0530

    user & role table set

--- common/models/userRole.js
+++ common/models/userRole.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = function(userTcode) {
+module.exports = function(userRole) {
 
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
